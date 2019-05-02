86efbfc4f331f8d56de4dc0fe3372904fab0f75a
watabee <watabee.dev@gmail.com>
2019-05-02 11:13:07 +0900

    Setup firebase

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,18 @@
+import * as firebase from "firebase/app";
+
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+
+class Firebase {
+  constructor() {
+    firebase.initializeApp(firebaseConfig);
+  }
+}
+
+export default Firebase;
