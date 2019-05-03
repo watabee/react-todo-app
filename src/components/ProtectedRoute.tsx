8c12ba29f6b36374c7e07d6a016121146f7633be
watabee <watabee.dev@gmail.com>
2019-05-03 09:26:36 +0900

    Fix ‘ProtectedRoute’

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,18 +1,10 @@
-import React, { Component, useContext, FC } from "react";
+import React, { useContext, FC } from "react";
 import { Route, Redirect, RouteProps } from "react-router";
 import Firebase, { FirebaseContext } from "../firebase";
 
-const ProtectedRoute: React.FC<RouteProps> = ({ component, ...rest }) => {
+const ProtectedRoute: React.FC<RouteProps> = props => {
   const firebase = useContext(FirebaseContext) as Firebase;
-
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        firebase.isLoggedIn() ? <Component {...props} /> : <Redirect to="/" />
-      }
-    />
-  );
+  return firebase.isLoggedIn() ? <Route {...props} /> : <Redirect to="/" />;
 };
 
 export default ProtectedRoute;
