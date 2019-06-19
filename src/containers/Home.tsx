784ba9cd2434696d854695ad01f0023311fade2e
watabee <watabee.dev@gmail.com>
2019-06-19 21:12:39 +0900

    Bye bye mapStateToProps and mapDispatchToProps in Home

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,30 +1,16 @@
 import React, { useEffect } from "react";
 
-import { homeActions } from "../redux/modules/home";
+import { homeActions, HomeState } from "../redux/modules/home";
 import { AppState } from "../redux/state";
-import { Dispatch, bindActionCreators } from "redux";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import useReactRouter from "use-react-router";
 
-interface StateProps {
-  redirectToLogin: boolean;
-  redirectToTodos: boolean;
-}
-
-interface DispatchProps {
-  waiting: () => void;
-  resetState: () => void;
-}
-
-type EnhancedProps = StateProps & DispatchProps;
-
-const HomeContainer: React.FC<EnhancedProps> = ({
-  redirectToLogin,
-  redirectToTodos,
-  waiting,
-  resetState
-}) => {
+const HomeContainer: React.FC = () => {
   const { history } = useReactRouter();
+  const { redirectToLogin, redirectToTodos } = useSelector<AppState, HomeState>(
+    state => state.home
+  );
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (redirectToLogin) {
@@ -32,30 +18,15 @@ const HomeContainer: React.FC<EnhancedProps> = ({
     } else if (redirectToTodos) {
       history.replace("/todos");
     } else {
-      waiting();
+      dispatch(homeActions.waiting());
     }
 
-    return () => resetState();
-  }, [redirectToLogin, redirectToTodos]);
+    return () => {
+      dispatch(homeActions.resetState());
+    };
+  }, [dispatch, history, redirectToLogin, redirectToTodos]);
 
   return <></>;
 };
 
-const mapStateToProps = (state: AppState): StateProps => ({
-  redirectToLogin: state.home.redirectToLogin,
-  redirectToTodos: state.home.redirectToTodos
-});
-
-const mapDispatchToProps = (dispatch: Dispatch): DispatchProps =>
-  bindActionCreators(
-    {
-      waiting: () => homeActions.waiting(),
-      resetState: () => homeActions.resetState()
-    },
-    dispatch
-  );
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(HomeContainer);
+export default HomeContainer;
